640be4d2d4540178fc1212dd3fa71a54e596a50f
sykong <sykong@syscon.kr>
2021-06-18 10:30:21 +0900

    mp3 플레이

--- Android/01Fundamental/2021/02SampleView/app/src/main/java/com/example/sampleview/MainActivity.java
+++ Android/01Fundamental/2021/02SampleView/app/src/main/java/com/example/sampleview/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.sampleview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        setContentView(new SampleView(this));   // view setting
+
+        mp = MediaPlayer.create(this, R.raw.preview);
+        mp.setLooping(false);
+        tv = (TextView) this.findViewById(R.id.hellobutton);
+        tv.setOnClickListener(new Button.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(!mp.isPlaying()){
+                    mp.start();
+                    tv.setText("Stop");
+                }else{
+                    mp.pause();
+                    tv.setText("Start");
+                }
+            }
+        });
     }
 }
