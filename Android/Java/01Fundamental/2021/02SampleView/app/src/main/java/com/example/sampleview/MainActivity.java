4d1db51eda85b6f679fb187ed82e1a78d2e4b519
sykong <sykong@syscon.kr>
2021-06-21 09:29:24 +0900

    Activity 전환, intent 값 전달, 버튼 set

--- Android/Java/01Fundamental/2021/02SampleView/app/src/main/java/com/example/sampleview/MainActivity.java
+++ Android/Java/01Fundamental/2021/02SampleView/app/src/main/java/com/example/sampleview/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.sampleview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,22 +18,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        setContentView(new SampleView(this));   // view setting
+//      setContentView(new SampleView(this));   // view setting
 
+        Intent intent = getIntent();
+        String start = intent.getExtras().getString("start");
+        String stop = intent.getExtras().getString("stop");
+
+/**
+ *  mp3 재생
+ */
+        setContentView(R.layout.activity_main);
         mp = MediaPlayer.create(this, R.raw.preview);
         mp.setLooping(false);
-        tv = (TextView) this.findViewById(R.id.hellobutton);
+        tv = (TextView) this.findViewById(R.id.main);
         tv.setOnClickListener(new Button.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 if(!mp.isPlaying()){
                     mp.start();
-                    tv.setText("Stop");
+                    tv.setText(stop);
                 }else{
                     mp.pause();
-                    tv.setText("Start");
+                    tv.setText(start);
                 }
             }
         });
