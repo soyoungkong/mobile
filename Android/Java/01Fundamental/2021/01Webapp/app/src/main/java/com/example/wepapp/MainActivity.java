2de9818addfee8f8823c2ef12b626cbd9023fb7a
soyoungkong <soyoungkong@gmail.com>
2021-10-07 13:52:32 +0900

    내부로 웹뷰 실행 & 뒤로가기

--- Android/Java/01Fundamental/2021/01Webapp/app/src/main/java/com/example/wepapp/MainActivity.java
+++ Android/Java/01Fundamental/2021/01Webapp/app/src/main/java/com/example/wepapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.wepapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,18 +26,45 @@ public class MainActivity extends AppCompatActivity {
         setTitle(getString(R.string.title_web_info));
 
         // WebView setting
+        // webView 가져오기
         wWeb = (WebView) findViewById(R.id.webInfo);
         WebSettings set = wWeb.getSettings();
+        // java script 활성화
         set.setJavaScriptEnabled(true);
 
-        wWeb.getSettings().setDomStorageEnabled(true);
+        // WebChromeClient 설정 - 브라우저 이벤트 구현을 위해 필요
+        wWeb.setWebChromeClient(new WebChromeClient());
+        // WebViewClient 설정 - 새 페이지를 띄울 때 새 창이 아닌 현재 view에서 띄우기 위해 필요
+        wWeb.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // 새 페이지를 띄울 때 현재 view에서 띄우도록 설정
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
 
         String url = null;
         url = String.format("https://naver.com/");
+        // 웹 페이지 띄우기
         wWeb.loadUrl(url);
     }
 
-    class WebClient extends WebViewClient{
+    /**
+     * 웹 브라우저 ‘뒤로가기’ 효과
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode != KeyEvent.KEYCODE_BACK)
+            return super.onKeyDown(keyCode, event);
 
+        if (wWeb.canGoBack()) {
+            wWeb.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
+
 }
