9f009abb43276b44bbe0e80c824da56f5d7438d3
soyoungkong <soyoungkong@gmail.com>
2021-10-14 14:31:36 +0900

    04 ImageView&Toast

--- Android/Java/01Fundamental/2021/04ImageView/app/src/main/java/com/example/imageview/MainActivity.java
+++ Android/Java/01Fundamental/2021/04ImageView/app/src/main/java/com/example/imageview/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.imageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv_test = (ImageView) findViewById(R.id.iv_test);
+        iv_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Image clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
